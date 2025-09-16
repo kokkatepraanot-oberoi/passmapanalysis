c1b775c337d984924b08560d76823db4f4e928be
kokkatepraanot-oberoi <praanot.kokkate@oberoi-is.org>
2025-09-16 12:36:40 +0530

    Update ois_pass_app.py

--- ois_pass_app.py
+++ ois_pass_app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 pd.options.display.float_format = "{:.1f}".format
 
-st.set_page_config(page_title="OIS JVLR - MS PASS Dashboard", layout="wide")
+st.set_page_config(page_title="OIS JVLR - Secondary PASS Dashboard", layout="wide")
 
 # --------- CONFIG ---------
 
